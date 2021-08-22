9612719167ea7cc5626b22f07fac7197ef6bc779
Abhay H Kashyap <abhayastro1004@gmail.com>
2021-08-23 01:19:20 +0530

    🐛 (fix) Added Context API Call
    Add new api url for retrieving all context options for the tasks and
    protect api  from CROS attacks by replying to only known sources.

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.apiList, name="api-listOverview"),
     path('all-tasks/', views.taskList, name="All-Tasks"),
-    path('task/<int:pk>', views.viewTask, name="View-Task"),
+    path('context/', views.contextList, name="All-Contexts"),
+    path('task/<int:pk>/', views.viewTask, name="View-Task"),
     path('add-task/', views.addTask, name="Add Task"),
     path('update-task/<int:pk>/', views.updateTask, name="Update Task"),
     path('delete-task/<int:pk>/', views.deleteTask, name="Delete Task")
--- todo/api/views.py
+++ todo/api/views.py
@@ -3,7 +3,18 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import TaskSerializer
 from .serializers import ContextSerializer
-from .models import Task
+from .models import Context, Task
+
+
+def formatData(contexts):
+    context_name = Context.context_name
+    res = []
+    for i, context in enumerate(contexts):
+        res.append({
+            "name": context_name[context["name"]],
+            "id": i
+        })
+    return(res)
 
 
 @api_view(['GET'])
@@ -23,6 +34,14 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def contextList(request):
+    contexts = Context.objects.all()
+    serializer = ContextSerializer(contexts, many=True)
+    formatData(serializer.data)
+    return Response(formatData(serializer.data))
 
 
 @api_view(['GET'])
